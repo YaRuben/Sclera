9470c1e4be86f3e1bc9e29d01e15f8a615acb2c6
yaRuben <ruben.mikaelian@gmail.com>
2021-07-29 12:31:26 -0700

    Milestone7.29.2021(refactoring)

--- public/js/utils.js
+++ public/js/utils.js
@@ -2,26 +2,44 @@
 
 const cfg = require('../../config');
 const Jwt = require('@hapi/jwt');
+const litedb = require('../../db/sqlite');
+const Boom = require('@hapi/boom');
 
-module.exports = { 
-    inspectObject: (o) => {return Object.entries(o);},
-    mapObject: (o) => {return new Map(Object.entries(o));},
-    properyValues: (o) => {return Object.values(o).reduce( (acc,v) => {acc += ` '${v}',`; return acc;},'').slice(0,-1)},
-    randomStrHex: (l) => {return require("crypto").randomBytes(l).toString('hex');},
+module.exports = {
+    inspectObject: (o) => { return Object.entries(o); },
+    mapObject: (o) => { return new Map(Object.entries(o)); },
+    properyValues: (o) => { return Object.values(o).reduce((acc, v) => { acc += ` '${v}',`; return acc; }, '').slice(0, -1) },
+    randomStrHex: (l) => { return require("crypto").randomBytes(l).toString('hex'); },
+    newToken: (u) => {
+        try {
+                let init = module.exports.randomStrHex(32);
+                const payload = { iss: 'HFR', iat: Date.parse(Date()) / 1000, aud: 'HFR', iss: 'HFRAS', user: `${u.user_login}`, role: `${u.user_role}`, nonce: `${u.user_init}` };
+                let tkn = Jwt.token.generate(payload, cfg.jwt.secret);
+                let sql = `update users set user_time=${Date.parse(Date()) / 1000}, user_token='${tkn}', user_init='${init}'  where user_id = ${u.user_id}`;
+                litedb.run(sql, []);
+                return tkn;
+            } 
+            catch(err){ 
+                throw Boom.notFound(err); 
+            }
+        
+    }
 
-    validateToken: async (artifacts, request, h) => {
-        let raw = artifacts.raw;
-        let decoded = artifacts.decoded;
-        let valid;
-        try{
-        await Jwt.token.verifySignature(artifacts, cfg.jwt.secret);
-        await Jwt.token.verifyTime(artifacts);
-        valid = true;    
-    }
-    catch(err){valid =false;} 
-        return {
-            isValid: valid,
-            credentials: { user: decoded.payload.user, role: decoded.payload.role }
-        };
-    }
+    // validateToken: async (artifacts, request, h) => {
+    //     let raw = artifacts.raw;
+    //     let decoded = artifacts.decoded;
+    //     let valid;
+    //     try{
+    //     await Jwt.token.verifySignature(artifacts, cfg.jwt.secret);
+    //     await Jwt.token.verifyTime(artifacts);
+    //     valid = true;    
+    // }
+    // catch(err){valid =false;} 
+    //     return {
+    //         isValid: valid,
+    //         credentials: { user: decoded.payload.user, role: decoded.payload.role }
+    //     };
+    // }
+
+
 }
--- routset.js
+++ routset.js
@@ -2,15 +2,15 @@
 const Path = require('path');
 const fs = require('fs');
 //const cript = require('@hapi/cryptiles');
-const util = require('./public/js/utils');
+const utils = require('./public/js/utils');
 const Jwt = require('@hapi/jwt');
 const cfg = require('./config');
 const litedb = require('./db/sqlite');
 const Mutex = require('async-mutex').Mutex;
 const Semaphore = require('async-mutex').Semaphore;
 const withTimeout = require('async-mutex').withTimeout;
-const utils = require('./public/js/utils');
-const Boom = require('@hapi/boom'); 
+const Boom = require('@hapi/boom');
+
 
 
 module.exports = [
@@ -34,25 +34,30 @@ module.exports = [
     },
     {
         method: 'GET',
-        path: '/token/{usr}/{pwd}',
+        path: '/login/{usr}/{pwd}',
         options: { auth: false },
         handler: async (request, h) => {
-            try{                
+            try {
                 let usr = request.params.usr;
                 let pwd = request.params.pwd;
-                let res = litedb.get(cfg.dblite.userlogin,[usr,pwd]);
-                let init = utils.randomStrHex(32);
-                if(res){
-                    const payload = {iss: 'HFR', iat: Date.parse(Date())/1000, aud: 'HFR', iss: 'HFRAS',  user: `${res.user_login}`, role: `${res.user_role}`, nonce: `${res.user_init }`};
-                    let tkn = Jwt.token.generate(payload, cfg.jwt.secret);
-                    let sql = `update users set user_time=${Date.parse(Date())/1000}, user_token='${tkn}', user_init='${init}'`;
-                    litedb.run(sql,[]);
-                    return h.response(tkn);
-                }else{throw 'No such user.';}                
-            }catch(err){
-               console.log(err); 
-               return h.response(err)
+                let res = litedb.get(cfg.dblite.userlogin, [usr, pwd]);
+                return h.response(utils.newToken(res));
+            } catch (err) {
+                throw Boom.unauthorized(`invalid credentials`);
             }
+
+            //     if(res){
+            //         let init = utils.randomStrHex(32);
+            //         const payload = {iss: 'HFR', iat: Date.parse(Date())/1000, aud: 'HFR', iss: 'HFRAS',  user: `${res.user_login}`, role: `${res.user_role}`, nonce: `${res.user_init }`};
+            //         let tkn = Jwt.token.generate(payload, cfg.jwt.secret);
+            //         let sql = `update users set user_time=${Date.parse(Date())/1000}, user_token='${tkn}', user_init='${init}'  where user_id = ${res.user_id}`;
+            //         litedb.run(sql,[]);
+            //         return h.response(tkn);
+            //     }else{throw 'No such user.';}                
+            // }catch(err){
+            //    console.log(err); 
+            //    return h.response(err)
+            // }
         }
     },
     {
@@ -60,24 +65,28 @@ module.exports = [
         path: '/refresh',
         options: { auth: false },
         handler: async (request, h) => {
-            try{
-            let hdrs = request.raw.req.headers;
-            let tkn = hdrs.authorization.replace('Bearer ', '');;
-            let res = litedb.get(cfg.dblite.userByToken, [`${tkn}`]);
-            let init = utils.randomStrHex(32);
-            if(res){
-                const payload = {iss: 'HFR', iat: Date.parse(Date())/1000, aud: 'HFR', iss: 'HFRAS',  user: `${res.user_login}`, role: `${res.user_role}`, nonce: `${init}`};
-                tkn = Jwt.token.generate(payload, cfg.jwt.secret);
-                let sql = `update users set user_time=${Date.parse(Date())/1000}, user_token='${tkn}', user_init='${init}'`;
-                litedb.run(sql,[]);
-                return h.response(tkn);
-            } else{throw 'No such session.';} 
-        }catch(err)
-            {
-                console.log(err); 
-                // return h.response(err).status(400);
+            try {
+                let hdrs = request.raw.req.headers;
+                let tkn = hdrs.authorization.replace('Bearer ', '');;
+                let res = litedb.get(cfg.dblite.userByToken, [`${tkn}`]);
+                return h.response(utils.newToken(res));
+            } catch (err) {
                 throw Boom.notFound(err);
-            };   
+            }
+            // let init = utils.randomStrHex(32);
+            // if(res){
+            //     const payload = {iss: 'HFR', iat: Date.parse(Date())/1000, aud: 'HFR', iss: 'HFRAS',  user: `${res.user_login}`, role: `${res.user_role}`, nonce: `${init}`};
+            //     tkn = Jwt.token.generate(payload, cfg.jwt.secret);
+            //     let sql = `update users set user_time=${Date.parse(Date())/1000}, user_token='${tkn}', user_init='${init}'  where user_id = ${res.user_id}`;
+            //     litedb.run(sql,[]);
+            //     return h.response(tkn);
+            // } else{throw 'No such session.';} 
+            // }catch(err)
+            //     {
+            //         console.log(err); 
+            //         // return h.response(err).status(400);
+            //         throw Boom.notFound(err);
+            //     };   
         }
     },
     {
@@ -86,28 +95,33 @@ module.exports = [
         options: { auth: false },
         handler: async (request, h) => {
             let res;
-            let tkn;
-            let rsp;
-            let extm = Date.parse(Date())/1000;
-            let init = utils.randomStrHex(32);
             try{
-            const pl = request.payload;
-            res = litedb.get(cfg.dblite.userlogin, [pl.email,pl.pwd]);
-            if(res){
-                const payload = {iss: 'HFR', iat: extm, aud: 'HFR', iss: 'HFRAS',  user: `${res.user_login}`, role: `${res.user_role}`, nonce: `${res.user_init }`};
-                tkn = Jwt.token.generate(payload, cfg.jwt.secret);
-                rsp = h.response(tkn);
-            } else 
-            {
-                rsp = h.redirect('error.html');
-            };   
-            console.log(`${tkn}\n ${JSON.stringify(Jwt.token.decode(tkn))}`);
-            let sql = `update users set user_time=${extm}, user_token='${tkn}', user_init='${init}'`;
-            litedb.run(sql,[]);
+                const pl = request.payload;
+                res = litedb.get(cfg.dblite.userlogin, [pl.email, pl.pwd]);
+                return h.response(utils.newToken(res));
+            }catch(err){
+                throw Boom.unauthorized(`invalid credentials`);
+               // res = litedb.get(cfg.dblite.userlogin, [pl.email, pl.pwd]);  
             }
-            catch(err){console.log(err); rsp = h.response(err);}
-            return rsp.header('Authorization',`Bearer ${tkn}`);
-            
+            //let extm = Date.parse(Date()) / 1000;
+            //let init = utils.randomStrHex(32);
+            // try {
+            //     const pl = request.payload;
+            //     res = litedb.get(cfg.dblite.userlogin, [pl.email, pl.pwd]);
+            //     if (res) {
+            //         const payload = { iss: 'HFR', iat: extm, aud: 'HFR', iss: 'HFRAS', user: `${res.user_login}`, role: `${res.user_role}`, nonce: `${res.user_init}` };
+            //         tkn = Jwt.token.generate(payload, cfg.jwt.secret);
+            //         rsp = h.response(tkn);
+            //     } else {
+            //         rsp = h.redirect('error.html');
+            //     };
+            //     console.log(`${tkn}\n ${JSON.stringify(Jwt.token.decode(tkn))}`);
+            //     let sql = `update users set user_time=${extm}, user_token='${tkn}', user_init='${init}'  where user_id = ${res.user_id}`;
+            //     litedb.run(sql, []);
+            // }
+            // catch (err) { console.log(err); rsp = h.response(err); }
+            // return rsp.header('Authorization', `Bearer ${tkn}`);
+
         }
 
     },
@@ -120,21 +134,21 @@ module.exports = [
             const ts = Date.now();
             const pl = request.payload;
             litedb.begin();
-            try{
+            try {
                 //user_status, user_role, user_login, user_pwd, user_time, user_created
-            res = litedb.run(cfg.dblite.usrinsert,[pl.role, pl.email,null,ts,ts]);
-            let uid = res.lastInsertRowid;
-            if (res.changes !== 1) {throw new Error('User insert failed. Rolling back.');}
-            let sql = `INSERT INTO profiles values (${uid},0,'${pl.fname}','${pl.lname}', '${pl.suff}', '${pl.company}', '${pl.position}',  '${pl.companyWeb}', '${pl.contactPhone}', '${pl.country}', '${pl.city}', '${pl.state}', '${pl.addr}')`;
-            res = litedb.run(sql,[]);
-            litedb.commit(); 
+                res = litedb.run(cfg.dblite.usrinsert, [pl.role, pl.email, null, ts, ts]);
+                let uid = res.lastInsertRowid;
+                if (res.changes !== 1) { throw new Error('User insert failed. Rolling back.'); }
+                let sql = `INSERT INTO profiles values (${uid},0,'${pl.fname}','${pl.lname}', '${pl.suff}', '${pl.company}', '${pl.position}',  '${pl.companyWeb}', '${pl.contactPhone}', '${pl.country}', '${pl.city}', '${pl.state}', '${pl.addr}')`;
+                res = litedb.run(sql, []);
+                litedb.commit();
             }
-            catch(err){
+            catch (err) {
                 litedb.rollback();
                 return h.response(JSON.stringify(err));
             }
             return h.response(JSON.stringify(res));
-        }  
+        }
     }
 ];
 exports.routeset;
